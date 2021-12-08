b5720a1f92bf25077385e83598b9d5b74714c46d
wiamebouaboub <wiame.bouaboub@etud.univ-jfc.fr>
2021-12-08 12:28:31 +0100

    gestion conflit event

--- src/test/java/agenda/EventTest.java
+++ src/test/java/agenda/EventTest.java
@@ -4,8 +4,12 @@ import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 
 public class EventTest {
     // November 1st, 2020
@@ -28,6 +32,22 @@ public class EventTest {
     // November 1st, 2020, 22:30, 120 minutes
     Event overlapping = new Event("Overlapping event", nov_1__2020_22_30, min_120);
 
+    @BeforeAll
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterAll
+    public static void tearDownClass() throws Exception {
+    }
+
+    @BeforeEach
+    public void setUp() throws Exception {
+    }
+
+    @AfterEach
+    public void tearDown() throws Exception {
+    }
+
     @Test
     public void eventIsInItsStartDay() {
         assertTrue(simple.isInDay(nov_1_2020), "Un événement a lieu dans son jour de début");
@@ -48,6 +68,63 @@ public class EventTest {
     @Test
     public void toStringShowsEventTitle() {
         assertTrue(simple.toString().contains("Simple event"), "toString() doit montrer le titre de l'événements");
+    }
+
+    /**
+     * Test of isInDay method, of class Event.
+     */
+    @Test
+    public void testIsInDay() {
+        System.out.println("isInDay");
+        LocalDate aDay = null;
+        Event instance = null;
+        boolean expResult = false;
+        boolean result = instance.isInDay(aDay);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getTitle method, of class Event.
+     */
+    @Test
+    public void testGetTitle() {
+        System.out.println("getTitle");
+        Event instance = null;
+        String expResult = "";
+        String result = instance.getTitle();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getStart method, of class Event.
+     */
+    @Test
+    public void testGetStart() {
+        System.out.println("getStart");
+        Event instance = null;
+        LocalDateTime expResult = null;
+        LocalDateTime result = instance.getStart();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getDuration method, of class Event.
+     */
+    @Test
+    public void testGetDuration() {
+        System.out.println("getDuration");
+        Event instance = null;
+        Duration expResult = null;
+        Duration result = instance.getDuration();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
     }
     
 }
